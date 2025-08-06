8c53f68c14785a5fc3483add63dc1f16d6a88cce
cy <94784880+cysuna@users.noreply.github.com>
2025-08-06 17:49:05 +0800

    Update llava_caption_sentence_judge_stage1.sh

--- llava_caption_sentence_judge_stage1.sh
+++ llava_caption_sentence_judge_stage1.sh
@@ -5,9 +5,9 @@ do
   for i in {0..7}
   do
     CUDA_VISIBLE_DEVICES=$i python llava_caption_sentence_judge_stage1.py \
-    --answers-file exp/llava-out1/coco-blip$i-$j.jsonl \
+    --answers-file exp/llava-out1/coco-llava157$i-$j.jsonl \
     --image-folder coco/train2014 \
-    --question-file exp/llava/coco-blip$i.jsonl \
+    --question-file exp/llava/coco-llava157$i.jsonl \
     --part $j &
   done
 done
